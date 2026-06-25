89eda9646e706ee1b78d876c4264af7b5bf655f5
Yang Zhang <z5531036@ad.unsw.edu.au>
2026-06-25 17:17:30 +0800

    com/study/p15/Main.java

--- com/study/p15/Main.java
+++ com/study/p15/Main.java
@@ -13,11 +13,11 @@ public class Main {
         int score = scanner.nextInt();
 
         // TODO：90 及以上 A，80-89 B，60-79 C，否则 D。
-        if (false) {
+        if (score >= 90) {
             System.out.println("A");
-        } else if (false) {
+        } else if (80 <= score && score <= 89) {
             System.out.println("B");
-        } else if (false) {
+        } else if (score >= 60 && score <= 79) {
             System.out.println("C");
         } else {
             System.out.println("D");
