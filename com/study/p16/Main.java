05922c0ce36bee1a551365826c06a31405242e00
Yang Zhang <z5531036@ad.unsw.edu.au>
2026-06-25 17:17:30 +0800

    com/study/p16/Main.java

--- com/study/p16/Main.java
+++ com/study/p16/Main.java
@@ -16,7 +16,24 @@ public class Main {
             case 1:
                 System.out.println("星期一");
                 break;
-            // TODO：补全 2-7 的 case。
+            case 2:
+                System.out.println("星期二");
+                break;
+              case 3:
+                System.out.println("星期三");
+                break;
+              case 4:
+                System.out.println("星期四");
+                break;
+              case 5:
+                System.out.println("星期五");
+                break;
+              case 6:
+                System.out.println("星期六");
+                break;
+              case 7:
+                System.out.println("星期日");
+                break;
             default:
                 System.out.println("输入有误");
         }
